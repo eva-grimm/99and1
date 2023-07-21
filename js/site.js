9149bc8d3ff008e9256fa9d4d94ff04dfe9cf92b
Cadence Eva Custin <eva.custin@gmail.com>
2023-07-21 15:56:16 -0400

    cleaned up code finished write up

--- js/site.js
+++ js/site.js
@@ -14,23 +14,31 @@ function getValues() {
             backdrop: false
         });
     } else if (startNumber > endNumber) {
-        // if we have a starting number greater than the ending number 
+        // if we have a starting value greater than the ending value 
         Swal.fire({
             title: 'Oops!',
-            text: 'The starting number must be less than the ending number',
+            text: 'The starting value must be less than the ending value',
             icon: 'error',
             backdrop: false
         });
-    } else if (endNumber > 100) {
-        // if we have a ending number greater than 100
+    } else if (startNumber < 1) {
+        // if we have a starting value less than 1
         Swal.fire({
             title: 'Oops!',
-            text: 'Sorry, but no ending number greater than 100',
+            text: 'Sorry, but no starting value less than 1',
+            icon: 'error',
+            backdrop: false
+        });
+    } else if (endNumber > 5000) {
+        // if we have a ending value greater than 5000
+        Swal.fire({
+            title: 'Oops!',
+            text: 'Sorry, but no ending value greater than 5,000',
             icon: 'error',
             backdrop: false
         });
     } else {
-        // create an array of the numbers in range
+        // create an array of numbers within the range
         let range = generateNumbers(startNumber, endNumber);
 
         // display the numbers in range
@@ -39,44 +47,48 @@ function getValues() {
 }
 
 // Creates every number in the chosen input range
-function generateNumbers(start, end) {
-    let range = [];
+function generateNumbers(startNumber, endNumber) {
+    let numbersToDisplay = [];
 
     // create an array including only the numbers in range
-    for (let number = start; number <= end; number++) {
-        range.push(number);
+    for (let i = startNumber; i <= endNumber; i++) {
+        numbersToDisplay.push(i);
     }
 
-    return range;
+    return numbersToDisplay;
 }
 
 // Puts the numbers on the page
 function displayNumbers(numbersToDisplay) {
     let tableHtml = '';
-    let tableRowHtml = '';
+    let currentHtmlString = '';
 
     for (let i = 0; i < numbersToDisplay.length; i++) {
+        // wipe currentHtmlString clean
+        currentHtmlString = '';
+        // set currentNumber to the next number in the array
         let currentNumber = numbersToDisplay[i];
 
-        // if the current number is one greater than a number divisible by 5, 
-        // or is otherwise the number 1, then begin a new row
+        // if currentNumber is one greater than a number divisible by 5, 
+        // or otherwise the number 1, then begin a new row
         if (currentNumber % 5 == 1) {
-            tableRowHtml = `<tr>`;
+            currentHtmlString = `<tr>`;
         }
 
-        // if the current number is divisible by 2, then make it bold
+        // if currentNumber is divisible by 2, then make it bold
         if (currentNumber % 2 == 0) {
-            tableRowHtml = `<td><strong>${currentNumber}<strong></td>`
+            currentHtmlString += `<td><strong>${currentNumber}<strong></td>`
         } else {
-            tableRowHtml = `<td>${currentNumber}</td>`
+            currentHtmlString += `<td>${currentNumber}</td>`
         }
 
-        // if the current number is disivible by 5, then end the row
+        // if currentNumber is divisible by 5, then end the row
         if (currentNumber % 5 == 0) {
-            tableRowHtml += `</tr>`;
+            currentHtmlString += `</tr>`;
         }
 
-        tableHtml += tableRowHtml;
+        // append the string the loop's been building to tableHtml 
+        tableHtml += currentHtmlString;
     }
 
     document.getElementById('results').innerHTML = tableHtml;
